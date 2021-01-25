215ddcc6be3f538cb7de132456c9e2f6c7e932d6
xXblackcatXx <77992542+xXblackcatXx@users.noreply.github.com>
2021-01-26 00:01:51 +0330

    Add files via upload
    this is a code which tests the Goldbach's conjecture 
    the Goldbach's conjecture says:
    every even number wich is bigger than 2 can be written as a sum of two even numbers

---
+++ final goldbach -G3.py
@@ -0,0 +1,27 @@
+tn=4
+
+while True:
+    ns=[]
+    x=tn/2
+    x=int(x)
+    for i in range (x):
+        a=0
+        b=0
+        m=x-i
+        n=x+1
+        for i in range(2,n):
+            if n % i == 0 :
+                a=1
+                break
+        for i in range(2,m):
+            if m % i == 0 :
+                a=1
+                break
+        if a==1 and b==1 :
+            print('we can show the number ',tn,' as plus of two prime numbers')
+            break
+    tn=tn+2
+            
+        
+            
+    
